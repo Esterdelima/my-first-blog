3bb7956a58ed80e2514c4632d8d50229b5866077
Esterdelima <ester_delima@hotmail.com>
2018-11-24 16:23:04 -0200

    meu primeiro site djangogirls

---
+++ teste.py
@@ -0,0 +1,120 @@
+"""print('Hello, Django girl')
+if 3>2:
+	print('funciona')
+	if 5<2:
+		print('5 é amior que 2')
+	else:
+		print('5´não é maior que 2')
+		name= 'Sonja'
+		if name== 'Ola':
+			print('ola ola!')
+		elif name == 'Sonja':
+			print ('Ola Sonja')
+		else:
+			print('Ola estranho!')
+
+#Mudar o volume muito alto ou muito baixo
+volume= 57
+if volume < 20:
+	print ("está um pouco baixo")
+elif 20 <= volume < 40:
+	print ("está bom para música ambiente")
+elif 40 <= volume < 60:
+	print ("perfeito, posso ouvir todos os detalhes")
+elif 60 <= volume < 80:
+	print ("ótimo para festa")
+elif 80 <= volume < 100:
+	print ("está muito alto!")
+else:
+	print ("meus ouvidos doem! :(")
+
+def oi():
+	print('ola!')
+	print('tudo bem')
+
+
+oi()
+
+def oi (nome):
+	if nome == 'ola':
+		print ('ola ola!')
+	elif nome == 'sonja!':
+		print('ola sonja!')
+	else:
+		print ('ola estranho!')
+
+oi("sonja")
+def oi(name):
+	print('ola' + name + '!')
+
+oi ("Rachel")"""
+
+def oi (nome):
+	print('oi '+ nome+ '!')
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'você']
+for name in girls:
+	oi(name)
+	print('proxima')
+
+oi('ester')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
